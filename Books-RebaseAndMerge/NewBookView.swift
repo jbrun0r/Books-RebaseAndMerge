2c6aa5f809bd0a6a6505b794047a607feff9f7db
João Bruno <joaobruno.rf@gmail.com>
2024-05-14 17:06:36 -0300

    feat: add New Book

---
+++ Books-RebaseAndMerge/NewBookView.swift
@@ -0,0 +1,47 @@
+//
+//  NewBookView.swift
+//  Books-RebaseAndMerge
+//
+//  Created by João Bruno Rodrigues on 14/05/24.
+//
+
+import SwiftUI
+
+struct NewBookView: View {
+    
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title: String = .empty
+    @State private var author: String = .empty
+    @State private var publisher: String = .empty
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Title", text: $title)
+                TextField("Author", text: $author)
+                TextField("Publisher", text: $publisher)
+                Button("Create") {
+                    let newBook = Book(title: title, author: author, publisher: publisher)
+                }
+                .frame(maxWidth: .infinity)
+                .buttonStyle(.borderedProminent)
+                .disabled(title.isEmpty || author.isEmpty || publisher.isEmpty)
+                .navigationTitle("New book")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading){
+                        Button("Cancel"){
+                            dismiss()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NewBookView()
+}
